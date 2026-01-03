f0f9f95c9975d2b673a40ea964b80594b3ebe045
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-01-03 15:08:25 +0100

    feat: implement Vehicule class with properties and addVehicule persistence

---
+++ Classes/Vehicule.php
@@ -0,0 +1,132 @@
+<?php
+
+class Vehicule
+{
+
+    private $id;
+    private $marque;
+    private $modele;
+    private $prix_journalier;
+    private $image_url;
+    private $categorie_id;
+    private $transmission; // Auto ou Manuelle
+    private $carburant;     // Diesel, Essence, Électrique
+    private $nb_places;
+    private $disponible;
+    private $description;
+
+    // Constructeur
+    public function __construct($marque, $modele, $prix, $image, $cat_id, $transmission, $carburant, $places, $description, $dispo = true , $id = null)
+    {
+        $this->id = $id;
+        $this->marque = $marque;
+        $this->modele = $modele;
+        $this->prix_journalier = $prix;
+        $this->image_url = $image;
+        $this->categorie_id = $cat_id;
+        $this->transmission = $transmission;
+        $this->carburant = $carburant;
+        $this->nb_places = $places;
+        $this->disponible = $dispo;
+        $this->description = $description;
+    }
+
+    // --- GETTERS ---
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+    public function getModele()
+    {
+        return $this->modele;
+    }
+    public function getPrixJournalier()
+    {
+        return $this->prix_journalier;
+    }
+    public function getImageUrl()
+    {
+        return $this->image_url;
+    }
+    public function getCategorieId()
+    {
+        return $this->categorie_id;
+    }
+    public function getTransmission()
+    {
+        return $this->transmission;
+    }
+    public function getCarburant()
+    {
+        return $this->carburant;
+    }
+    public function getNbPlaces()
+    {
+        return $this->nb_places;
+    }
+    public function isDisponible()
+    {
+        return $this->disponible;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+    // --- SETTERS ---
+    public function setMarque($marque)
+    {
+        $this->marque = $marque;
+    }
+    public function setModele($modele)
+    {
+        $this->modele = $modele;
+    }
+    public function setPrixJournalier($prix)
+    {
+        $this->prix_journalier = $prix;
+    }
+    public function setImageUrl($image)
+    {
+        $this->image_url = $image;
+    }
+    public function setDisponible($bool)
+    {
+        $this->disponible = $bool;
+    }
+
+
+    public function setDescription($desc)
+    {
+        $this->description = $desc;
+    }
+
+    // MÉTHODES  
+
+    // Exemple : Méthode pour récupérer les voitures en vedette (statique)
+    public static function getFeaturedVehicles()
+    {
+        $db = DbConnection::getConnection();
+        $sql = "SELECT * FROM vehicules WHERE disponible = 1 LIMIT 3";
+        $stmt = $db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function addVehicule()
+    {
+
+        $pdo = DbConnection::getConnection();
+
+        $sql = "INSERT into vehicules (marque,modele,prix_par_jour,image,categorie_id,transmission,carburant,nb_places,description) 
+        values (?,?,?,?,?,?,?,?,?) ";
+
+        $stmt = $pdo->prepare($sql);
+        return $stmt->execute([$this->marque, $this->modele, $this->prix_journalier, $this->image_url, $this->categorie_id, $this->transmission, $this->carburant, $this->nb_places, $this->description]);
+    }
+}
