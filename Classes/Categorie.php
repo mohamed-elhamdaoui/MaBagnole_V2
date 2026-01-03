2ff33778dccdebce4e30bb8da1fbd1ba949dd0f5
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-01-03 19:13:06 +0100

    feat: add Categorie model for car classification

---
+++ Classes/Categorie.php
@@ -0,0 +1,44 @@
+<?php
+
+class Categorie
+{
+    private $id;
+    private $nom;
+    private $description;
+
+    public function __construct($nom, $description, $id = null)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->description = $description;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function setDescription($description){
+        $this->nom = $description;
+    }
+
+    // Méthode pour récupérer toutes les catégories (utile pour tes <select>)
+    public static function getAll()
+    {
+        $db = DbConnection::getConnection();
+        $stmt = $db->query("SELECT * FROM categories ORDER BY nom ASC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
